7baad6056aba3c0af0ca4b420a069513d75d3242
Israel Calderon de la Cruz <icalderond@outlook.com>
2019-06-19 15:27:15 -0500

    Add project files.

---
+++ xf.practices.notes/ViewModels/LoginViewModel.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace xf.practices.notes.ViewModels
+{
+    public class LoginViewModel : NotificationEnabledObject
+    {
+        public LoginViewModel()
+        {
+            Title = "Login";
+        }
+
+        private string _Title;
+
+        public string Title
+        {
+            get { return _Title; }
+            set
+            {
+                _Title = value;
+                OnPropertyChanged();
+            }
+        }
+    }
+}
---
+++ xf.practices.notes/ViewModels/NotificationEnabledObject.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Text;
+
+namespace xf.practices.notes.ViewModels
+{
+    public class NotificationEnabledObject:INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+        protected void OnPropertyChanged([CallerMemberName]string propertyName = null)
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }
+    }
+}
